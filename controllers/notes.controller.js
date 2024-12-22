c73839b0a61e90a1db8639ff8101e8d0f79c60e8
andikaPalian <andika12des@gmail.com>
2024-12-22 18:28:24 +0800

    menyelesaikan CRUD notes

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -24,4 +24,79 @@ const createNotes = async (req, res) => {
     };
 };
 
-module.exports = {createNotes};
+const getAllNotes = async (req, res) => {
+    try {
+        const {tags} = req.query;
+        const filters = {userId: req.user.id};
+        if (tags) {
+            filters.tags = {$in: tags.split(", ")};
+        };
+        const notes = await Notes.find(filters);
+        res.status(200).json({data: notes});
+    } catch (error) {
+        console.error("Error getting notes:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const getSingleNotes = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "Invalid Notes Id"});
+        };
+        const notes = await Notes.findById(req.params.id);
+        if (!notes) {
+            return res.status(404).json({message: "Notes not found"});
+        };
+        res.status(200).json({data: notes});
+    } catch (error) {
+        console.error("Error getting notes:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const updateNotes = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "Invalid Notes Id"});
+        };
+        const notes = await Notes.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if (!notes) {
+            return res.status(404).json({message: "Notes not found"});
+        };
+        res.status(200).json({message: "Notes updated successfully", data: notes});
+    } catch (error) {
+        console.error("Error updating notes:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const deleteNotes = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "Invalid Notes Id"});
+        };
+        const notes = await Notes.findByIdAndDelete(req.params.id);
+        if (!notes) {
+            return res.status(404).json({message: "Notes not found"});
+        };
+        res.status(200).json({message: "Notes deleted successfully"});
+    } catch (error) {
+        console.error("Error deleting notes:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+module.exports = {createNotes, getAllNotes, getSingleNotes, updateNotes, deleteNotes};
--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const validateToken = require("../middleware/authMiddleware");
-const { createNotes } = require("../controllers/notes.controller");
+const { createNotes, getAllNotes, getSingleNotes, updateNotes, deleteNotes } = require("../controllers/notes.controller");
 const router = express.Router();
 
 router.use(validateToken);
 router.post("/create", createNotes);
+router.get("/get", getAllNotes);
+router.get("/get/:id", getSingleNotes);
+router.put("/update/:id", updateNotes);
+router.delete("/delete/:id", deleteNotes)
 
 module.exports = router;
